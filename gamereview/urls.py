e0d6788f96498803b895b3efdba5d7f3b2d9cdc5
byteboyMW <micah.jacoby14@gmail.com>
2022-01-31 17:19:49 -0500

    Changing Directory Name

--- gamereview/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-app_name = 'test'
-
-urlpatterns = [
-    url(r'^$', views.TestView.as_view(),name='home'),
-]
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import url, include
-
+from django.urls import path, re_path
+from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'', include('gamereviews.urls'))
+    re_path(r'^$', views.TestView.as_view())
 ]
